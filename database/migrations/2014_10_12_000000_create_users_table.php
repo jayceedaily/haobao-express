74c271097f29f20afe8cbae399160bfafb17ce10
Jaycee Daily <alohajaycee@gmail.com>
2022-09-12 23:10:20 +0800

    Update user seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('pin')->nullable();
+
+            // Default Columns
+            $table->foreignId('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users');
+
+            $table->foreignId('updated_by')->nullable();
+            $table->foreign('updated_by')->references('id')->on('users');
+
+            $table->foreignId('deleted_by')->nullable();
+            $table->foreign('deleted_by')->references('id')->on('users');
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
