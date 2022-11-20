fc1cafef623ee770316397c312a78bdc006e0fa5
Jaycee Daily <alohajaycee@gmail.com>
2022-11-20 21:37:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,277 @@
+<template>
+  <div class="grid grid-cols-3 gap-3 p-10">
+    <div
+      v-for="(product, index) in products"
+      :key="index"
+      class="border rounded-lg"
+    >
+      <div class="p-5">
+        <p>
+          {{ product.name }}
+          <span v-if="product.variants && product.variants.length"
+            >({{ product.variants.length }} Variants)</span
+          >
+
+          <br />
+          <strong> P{{ priceResolver(product)[0] }} </strong>
+          <strong v-if="priceResolver(product)[1]">
+            - P{{ priceResolver(product)[1] }}
+          </strong>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+
+export default {
+  name: "App",
+
+  setup() {
+    const addOns = ref([
+      {
+        name: "Sweet & Sour Pork",
+        price: 85,
+      },
+      {
+        name: "Szechuan Pork",
+        price: 85,
+      },
+      {
+        name: "Wonton Dumplings",
+        price: 85,
+      },
+      {
+        name: "Shanghai Rolls - 6Pcs",
+        price: 85,
+      },
+      {
+        name: "Fried Chicken - 1Pc",
+        price: 65,
+      },
+      {
+        name: "Fried Egg",
+        price: 25,
+      },
+    ]);
+
+    const products = ref([
+      {
+        name: "Beef Wonton Mami",
+        price: 260,
+        category: "Specialty",
+      },
+      {
+        name: "Beef Mami",
+        price: 190,
+        category: "Specialty",
+      },
+      {
+        name: "Wonton Mami",
+        price: 175,
+        category: "Specialty",
+      },
+      {
+        name: "Wonton Soup",
+        price: 125,
+        category: "Specialty",
+      },
+      {
+        name: "Extra Noodles",
+        price: 50,
+        category: "Specialty",
+      },
+      {
+        name: "Fried Chicken",
+        variants: [
+          {
+            name: "4PC Fried Chicken - Classic",
+            price: 260,
+          },
+          {
+            name: "6PC Fried Chicken - Classic",
+            price: 390,
+          },
+          {
+            name: "8PC Fried Chicken - Classic",
+            price: 520,
+          },
+          {
+            name: "4PC Fried Chicken - Hao Bao Original",
+            price: 260,
+          },
+          {
+            name: "6PC Fried Chicken - Hao Bao Original",
+            price: 390,
+          },
+          {
+            name: "8PC Fried Chicken - Hao Bao Original",
+            price: 520,
+          },
+          {
+            name: "4PC Fried Chicken - Orange Chicken",
+            price: 260,
+          },
+          {
+            name: "6PC Fried Chicken - Orange Chicken",
+            price: 390,
+          },
+          {
+            name: "8PC Fried Chicken - Orange Chicken",
+            price: 520,
+          },
+          {
+            name: "4PC Fried Chicken - Honey Garlic",
+            price: 260,
+          },
+          {
+            name: "6PC Fried Chicken - Honey Garlic",
+            price: 390,
+          },
+          {
+            name: "8PC Fried Chicken - Honey Garlic",
+            price: 520,
+          },
+          {
+            name: "4PC Fried Chicken - Soy Garlic",
+            price: 260,
+          },
+          {
+            name: "6PC Fried Chicken - Soy Garlic",
+            price: 390,
+          },
+          {
+            name: "8PC Fried Chicken - Soy Garlic",
+            price: 520,
+          },
+          {
+            name: "4PC Fried Chicken - Szechuan",
+            price: 260,
+          },
+          {
+            name: "6PC Fried Chicken - Szechuan",
+            price: 390,
+          },
+          {
+            name: "8PC Fried Chicken - Szechuan",
+            price: 520,
+          },
+        ],
+      },
+      {
+        name: "Beef Brocolli",
+        price: 285,
+        category: "Platter",
+      },
+      {
+        name: "Sweet & Sour Pork",
+        price: 245,
+        category: "Platter",
+      },
+      {
+        name: "Szechuan Pork",
+        price: 245,
+        category: "Platter",
+      },
+      {
+        name: "Spring Rolls",
+        price: 145,
+        category: "Platter",
+      },
+      {
+        name: "Shanghai Rolls - 15Pcs",
+        price: 195,
+        category: "Platter",
+      },
+      {
+        name: "Stir Fried Bok Choy",
+        price: 195,
+        category: "Platter",
+      },
+      {
+        name: "Soy Garlic Spinach",
+        price: 120,
+        category: "Platter",
+      },
+      {
+        name: "Chicken Sandwich",
+        price: 85,
+        category: "Sandwich",
+      },
+      {
+        name: "Premium Leche Flan",
+        price: 85,
+        category: "Dessert",
+      },
+      {
+        name: "Shrimp Fried Rice - Platter",
+        price: 360,
+        category: "Fried Rice",
+      },
+      {
+        name: "Yang Chow - Platter",
+        price: 315,
+        category: "Fried Rice",
+      },
+      {
+        name: "Pork Chow Fan - Platter",
+        price: 265,
+        category: "Fried Rice",
+      },
+      {
+        name: "Hao Bao Egg Fried Rice - Platter",
+        price: 360,
+        category: "Fried Rice",
+      },
+      {
+        name: "Hao Bao Egg Fried Rice - Rice Bowl",
+        price: 85,
+        category: "Rice Bowls",
+        variants: addOns,
+      },
+      {
+        name: "Pork Chow Fan - Rice Bowl",
+        price: 95,
+        category: "Rice Bowls",
+        variants: addOns,
+      },
+    ]);
+
+    const priceResolver = (product) => {
+      let price = product.price ? [product.price] : [];
+
+      if (product.variants && product.variants.length) {
+        product.variants.forEach((_product) => {
+          console.log(_product.price, product.price);
+          price.push(
+            product.price ? _product.price + product.price : _product.price
+          );
+        });
+      }
+
+      let productPrice = [price.min()];
+
+      if (!productPrice.includes(price.max())) {
+        productPrice.push(price.max());
+      }
+
+      return productPrice;
+    };
+    return {
+      products,
+      priceResolver,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: "Rubik", sans-serif;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+Array.prototype.max = function () {
+  return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function () {
+  return Math.min.apply(null, this);
+};
+
+createApp(App).mount("#app");
