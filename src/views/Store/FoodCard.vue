f73043da64fd41ec8b5465b2f32544c922131bd2
Jaycee Daily <alohajaycee@gmail.com>
2023-01-11 13:32:22 +0800

    minor adjustment

--- src/views/Store/FoodCard.vue
+++ src/views/Store/FoodCard.vue
@@ -1,13 +1,17 @@
 <template>
   <div
-    :style="['background: url(' + image + ')', 'background-size: cover', 'background-position: center']"
-    class="rounded-xl h-64 shadow-sm border border-1  flex flex-col justify-between"
+    :style="[
+      'background: url(' + image + ')',
+      'background-size: cover',
+      'background-position: center',
+    ]"
+    class="rounded-xl shadow-sm border border-1 flex flex-col justify-between h-72"
   >
-    <div class="bg-gradient-to-b  from-black/70  p-3 rounded-t-xl h-32">
+    <div class="bg-gradient-to-b from-black/70 p-3 rounded-t-xl h-32">
       <h2 class="font-bold text-white text-xl drop-shadow-md">{{ name }}</h2>
       <!-- <span class="bg-white text-xs px-2 py-1 rounded text-red-500">🔥Best Seller</span> -->
     </div>
-    <div class="flex justify-end p-3 ">
+    <div class="flex justify-end p-3">
       <div
         class="rounded-full bg-white inline-block px-2 py-1 text-xs font-bold shadow-sm"
       >
--- src/views/Store/Index2.vue
+++ src/views/Store/Index2.vue
@@ -1,82 +1,80 @@
 <template>
-
-<div class="container mx-auto lg:w-1/3">
-
-  <nav class="sticky top-0 z-10">
-    <div class="bg-yellow-500 justify-between flex">
-      <div class="bg-yellow-400 p-4">
-        <Bars3Icon class="w-6" />
+  <div class="container mx-auto lg:w-1/3">
+    <nav class="sticky top-0 z-10">
+      <div class="bg-yellow-500 justify-between flex">
+        <div class="bg-yellow-400 p-4">
+          <Bars3Icon class="w-6" />
+        </div>
+        <div class="p-4">
+          <ShoppingBagIcon class="w-6" />
+        </div>
       </div>
-      <div class="p-4">
-        <ShoppingBagIcon class="w-6" />
-      </div>
-    </div>
-  </nav>
-  <div class="px-5 py-10">
-    <!-- STORE TITLE -->
-    <div class="mb-3">
-      <h1 class="font-bold text-2xl">Hao Bao Wonton Noodles</h1>
+    </nav>
+    <div class="px-3 py-10">
+      <!-- STORE TITLE -->
+      <div class="mb-3">
+        <h1 class="font-bold text-2xl">Hao Bao Wonton Noodles</h1>
 
-      <div class="flex gap-3">
-        <div class="text-yellow-500 flex">
+        <div class="flex gap-3">
+          <!-- <div class="text-yellow-500 flex">
           <StarIcon class="w-4" />
           <StarIcon class="w-4" />
           <StarIcon class="w-4" />
           <StarIcon class="w-4" />
           <StarIconOutline class="w-4" />
+        </div> -->
+          <p class="text-gray-400">2.4km away</p>
+
+          <p class="font-medium flex gap-1">
+            <ClockIcon class="w-4 inline-block text-yellow-500" />
+            <span>Delivery 45 min</span>
+          </p>
         </div>
-        <p class="text-gray-400">2.4km away</p>
-
-        <p class="font-medium flex gap-1">
-          <ClockIcon class="w-4 inline-block text-yellow-500" />
-          <span>Delivery 45 min</span>
-        </p>
       </div>
-    </div>
-    <!-- STORE TITLE END -->
+      <!-- STORE TITLE END -->
 
-    <!-- FILTER -->
-    <div class="mb-5 flex gap-2 overflow-auto">
-      <span class="bg-yellow-500 px-5 py-2 rounded-full text-sm font-medium"
-        >All</span
-      >
-      <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
-        >Popular</span
-      >
-      <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
-        >Chicken</span
-      >
-      <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
-        >Beef</span
-      >
-      <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
-        >Pork</span
-      >
-      <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
-        >Rice</span
-      >
-      <span
-        class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium whitespace-nowrap"
-        >Chow Mein</span
-      >
-      <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
-        >Mami</span
-      >
+      <!-- FILTER -->
+      <div class="mb-5 flex gap-2 overflow-auto hide-scroll">
+        <span class="bg-yellow-500 px-5 py-2 rounded-full text-sm font-medium"
+          >All</span
+        >
+        <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
+          >Popular</span
+        >
+        <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
+          >Chicken</span
+        >
+        <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
+          >Beef</span
+        >
+        <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
+          >Pork</span
+        >
+        <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
+          >Rice</span
+        >
+        <span
+          class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium whitespace-nowrap"
+          >Chow Mein</span
+        >
+        <span class="bg-gray-200 px-5 py-2 rounded-full text-sm font-medium"
+          >Mami</span
+        >
+      </div>
+      <!-- FILTER END -->
+      <!-- STORE ITEMS -->
+      <div class="grid grid-cols-2 gap-3">
+        <food-card
+          v-for="(item, index) in items"
+          :key="index"
+          :name="item.name"
+          :price="item.price"
+          :image="item.image"
+        />
+      </div>
+      <!-- STORE ITEMS END -->
     </div>
-    <!-- FILTER END -->
-    <!-- STORE ITEMS -->
-    <div class="grid grid-cols-2 gap-3">
-      <food-card
-        v-for="(item, index) in items"
-        :key="index"
-        :name="item.name"
-        :price="item.price"
-        :image="item.image"
-      />
-    </div>
-    <!-- STORE ITEMS END -->
   </div>
-</div>
 </template>
 
 <script>
