75dad8c39d7b97dd64f45713cde63fa55a855e17
Jaycee Daily <alohajaycee@gmail.com>
2023-01-19 23:27:21 +0800

    update logo and PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+
+import { VitePWA } from "vite-plugin-pwa";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ registerType: "autoUpdate", injectRegister: "auto" }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
