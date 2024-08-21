eef09e3b27399afe27dc784096660a08a49b2b22
Jaycee Daily <alohajaycee@gmail.com>
2024-08-21 11:07:40 +0800

    fix location

--- src/views/Location/Index.vue
+++ src/views/Location/Index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex gap-10 flex-col pb-20">
-    <div class="lg:w-2/3 md:w-3/4 xl:w-1/3 flex mx-auto ">
+  <div class="flex gap-10 flex-col pb-20 px-3">
+    <div class="lg:w-2/3 md:w-3/4 xl:w-1/3 flex mx-auto">
       <div>
         <div class="font-medium text-xl">
           Hao Bao - Angeles University Foundation (AUF) Branch
         </div>
-        <div class="text-blue-600 text-sm">Open 10AM - 9PM Everyday</div>
+        <div class="text-blue-600 text-sm">Open 11AM - 10:30PM Everyday</div>
         <div class="mb-3 text-sm text-gray-500">
           2nd Floor Unit B-3, One Kingfisher Business Center, Angeles, 2009
           Pampanga - Located in front of Angeles University Foundation, above LA
@@ -14,8 +14,7 @@
         <div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15404.920648103247!2d120.5952665!3d15.1456994!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f39cff2df76f%3A0x9a580cb406210994!2sHao%20Bao%20Wonton%20Noodles!5e0!3m2!1sen!2sph!4v1700564786187!5m2!1sen!2sph"
-            width="600"
-            height="450"
+            class="w-full h-[500px]"
             style="border: 0"
             allowfullscreen=""
             loading="lazy"
@@ -36,8 +35,7 @@
         <div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1925.586289755366!2d120.5943148!3d15.148864!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f38efa51497f%3A0x72daaf91e1a01694!2sHao%20Bao%20Wonton%20Noodles!5e0!3m2!1sen!2sph!4v1700565181090!5m2!1sen!2sph"
-            width="600"
-            height="450"
+            class="w-full h-[500px]"
             style="border: 0"
             allowfullscreen=""
             loading="lazy"
@@ -60,8 +58,7 @@
         <div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15408.173961639464!2d120.6201567!3d15.1009316!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f76c968586e7%3A0x57803d6619808bfd!2sHao%20Bao%20Wonton%20Noodles%20-%20Baliti!5e0!3m2!1sen!2sph!4v1723946961187!5m2!1sen!2sph"
-            width="600"
-            height="450"
+            class="w-full h-[500px]"
             style="border: 0"
             allowfullscreen=""
             loading="lazy"
