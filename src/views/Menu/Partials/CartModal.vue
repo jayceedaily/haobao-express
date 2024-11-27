fdea664622ff04f4d38fc840a4e37e053ed1aea9
Jaycee Daily <alohajaycee@gmail.com>
2024-11-28 00:01:03 +0800

    compute total and modifier

--- src/views/Menu/Partials/CartModal.vue
+++ src/views/Menu/Partials/CartModal.vue
@@ -2,81 +2,100 @@
   <hb-modal title="My Cart" no-padding>
     <template #content>
       <div class="flex flex-col gap-5">
-        <div
-          v-for="(item, index) in cart"
-          :key="index"
-          class="flex justify-between py-2 px-3"
-          :class="item.quantity == 0 ? 'bg-gray-100' : ''"
-        >
-          <div class="flex gap-5">
-            <img :src="item.item.image" class="w-24" />
-            <div :class="item.quantity == 0 ? 'line-through' : ''">
-              {{ item.item.name }}
-              <div>₱{{ item.item.price }}</div>
-            </div>
-          </div>
-          <div v-if="item.quantity == 0">
-            <div class="flex gap-3 h-full">
-              <button
-                class="px-3 py-1 bg-gray-200 text-gray-500 rounded-lg font-medium h-full"
-                @click="item.quantity++"
-              >
-                <XMarkIcon class="w-4 h-4" />
-              </button>
-              <button
-                class="px-3 py-1 bg-red-100 text-red-500 rounded-lg font-medium h-full"
-                @click="handleDelete(index)"
-              >
-                <TrashIcon class="w-4 h-4" />
-              </button>
-            </div>
-          </div>
-          <div v-else class="flex flex-col items-center">
-            <div class="flex gap-2">
-              <button
-                class="bg-gray-200 px-2 rounded-full"
-                @click="item.quantity--"
-              >
-                <MinusIcon class="h-3 w-3" />
-              </button>
-              {{ item.quantity }}
-              <button
-                class="bg-gray-200 px-2 rounded-full"
-                @click="item.quantity++"
-              >
-                <PlusIcon class="h-3 w-3" />
-              </button>
-            </div>
-            <div class="text-gray-400">
-              ₱{{ (item.item.price * item.quantity).toLocaleString() }}
-            </div>
-          </div>
-        </div>
+        <table>
+          <tr
+            v-for="(item, index) in cart"
+            :key="index"
+            class=""
+            :class="item.quantity == 0 ? 'bg-gray-100' : ''"
+          >
+            <td class="align-top w-20 py-2 px-2">
+              <img :src="item.item.image" class="" />
+            </td>
+            <td class="py-2">
+              <div :class="item.quantity == 0 ? 'line-through' : ''">
+                <span class="text-lg">
+                  {{ item.item.name }}
+                </span>
+                <div class="text-lg">₱{{ item.item.price }}</div>
+                <div
+                  v-for="(modifier, index) in item.item.modifiers"
+                  :key="index"
+                >
+                  <div
+                    v-for="(modifierItem, index) in modifier.items"
+                    :key="index"
+                  >
+                    <div v-if="modifierItem.selected" class="text-gray-400">
+                      +₱{{ modifierItem.price }} {{ modifierItem.name }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div v-if="item.quantity == 0">
+                <div class="flex gap-3 h-full">
+                  <button
+                    class="px-3 py-1 bg-gray-200 text-gray-500 rounded-lg font-medium h-full"
+                    @click="item.quantity++"
+                  >
+                    <XMarkIcon class="w-4 h-4" />
+                  </button>
+                  <button
+                    class="px-3 py-1 bg-red-100 text-red-500 rounded-lg font-medium h-full"
+                    @click="handleDelete(index)"
+                  >
+                    <TrashIcon class="w-4 h-4" />
+                  </button>
+                </div>
+              </div>
+              <div v-else class="flex flex-col items-center">
+                <div class="flex gap-2">
+                  <button
+                    class="bg-gray-200 px-2 rounded-full"
+                    @click="item.quantity--"
+                  >
+                    <MinusIcon class="h-3 w-3" />
+                  </button>
+                  {{ item.quantity }}
+                  <button
+                    class="bg-gray-200 px-2 rounded-full"
+                    @click="item.quantity++"
+                  >
+                    <PlusIcon class="h-3 w-3" />
+                  </button>
+                </div>
+                <div class="text-gray-400">
+                  ₱{{ getProductTotal(item).toLocaleString() }}
+                </div>
+              </div>
+            </td>
+          </tr>
+        </table>
       </div>
       <hr class="my-5" />
-	  <div class="px-5 pb-5">
+      <div class="px-5 pb-5">
+        <div v-if="cart.length == 0">Your Cart is Empty</div>
+        <div v-else class="flex flex-col gap-3">
+          <div class="flex justify-between">
+            <div class="font-medium">Delivery</div>
+            FREE
+          </div>
+          <div class="flex justify-between">
+            <div class="font-medium">Total</div>
 
-		  <div v-if="cart.length == 0">Your Cart is Empty</div>
-		  <div v-else class="flex flex-col gap-3">
-			<div class="flex justify-between">
-			  <div class="font-medium">Delivery</div>
-			  FREE
-			</div>
-			<div class="flex justify-between">
-			  <div class="font-medium">Total</div>
-	
-			  ₱{{ cartTotal.toLocaleString() }}
-			</div>
+            ₱{{ cartTotal.toLocaleString() }}
+          </div>
 
-			<button
+          <button
             class="bg-primary text-white font-medium px-5 py-3 rounded-lg"
           >
             Checkout
           </button>
-		  </div>
-	  </div>
+        </div>
+      </div>
     </template>
-
   </hb-modal>
 </template>
 
@@ -98,11 +117,25 @@ const cartTotal = computed(() => {
   let total = 0;
 
   props.cart.forEach((item) => {
-    total += item.quantity * item.item.price;
+    total += getProductTotal(item);
   });
 
   return total;
 });
+
+const getProductTotal = (product) => {
+  let total = product.quantity * product.item.price;
+
+  product.item.modifiers.forEach((modifier) => {
+    modifier.items.forEach((_item) => {
+      if (_item.selected) {
+        total += product.quantity * _item.price;
+      }
+    });
+  });
+
+  return total;
+};
 
 const handleDelete = (index) => {
   emit("delete", index);
